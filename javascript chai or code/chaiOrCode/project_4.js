d56966de7aad36c954befb747cbc4d496a213a0e
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2023-12-15 01:46:41 +0530

    problems related to project 4

---
+++ javascript chai or code/chaiOrCode/project_4.js
@@ -0,0 +1,96 @@
+let ramdomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessfield')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const loOrHi = document.querySelector('.loOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+if(playGame){
+    submit.addEventListener('click' , function(e){
+        e.preventDefault()
+      const guess =  parseInt( userInput.value)
+      console.log(guess);
+      validateGuess(guess)
+    })
+}
+
+function validateGuess(guess){
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    } else if(guess < 1){
+        alert('Please enter a number more than 1r') 
+    }else if(guess > 100 ){
+        alert('Please enter a number less than 100')
+    }else{
+        prevGuess.push(guess)
+        if(numGuess ===11){
+            displayGuess(guess)
+            displayMessage(`Game Over . Random number was ${ramdomNumber}`)
+            endGame()
+        }else {
+            displayGuess(guess) 
+            checkGuess(guess)
+        }
+    }
+}
+
+function checkGuess(guess){
+if(guess === ramdomNumber){
+    displayMessege('You guessed is right')
+    endGame()
+} else if(guess < ramdomNumber){
+    displayMessege('Number is toooo low')
+}else if(guess > ramdomNumber){
+    displayMessege('Number is tooooo high')
+}
+
+}
+
+function displayGuess(guess){
+    userInput.value = ''
+    guessSlot.innerHTML +=    `${guess}   `
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`
+}
+
+function displayMessege(message){
+        loOrHi.innerHTML = `<h2> ${message}</h2>`
+}
+
+function endGame(){
+    userInput.value = ''
+    userInput.setAttribute('disabled' , '')
+    p.classList.add('button')
+    p.innerHTML = `<h2 id = "newGame">Start new Game </h2>`
+    startOver.appendChild(p)
+    playGame = false;
+    newGame()
+}
+
+function newGame(){
+    const newGameButton = document.querySelector('#newGame')
+    newGameButton.addEventListener('click' , function(e){
+        ramdomNumber =  parseInt(Math.random() * 100 + 1);
+        prevGuess = []
+        numGuess = 1
+        guessSlot.innerHTML = ''
+        remaining.innerHTML =  `${11 - numGuess}`
+        userInput.removeAttribute('disabled')
+        startOver.removeChild(p)
+        playGame = true
+    })
+
+}
+
+
+
+
