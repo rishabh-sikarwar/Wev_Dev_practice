f778de0c9984c078c1fa683aa3417c33375a0ca7
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2024-01-08 21:57:57 +0530

    prototype episode 43

--- javascript chai or code/chaiOrCode/advance javascript/OOP_1.js
+++ javascript chai or code/chaiOrCode/advance javascript/OOP_1.js
@@ -1,7 +1,59 @@
+//Object literals   and  "this" keyword 
+
 const user = {
     usrname : "Rishabh",
     loginCount : 8,
-    signedIn: true
+    signedIn: true,
+    getUserDeatailes:  function(){
+        //console.log("got user detailes from the database");      *******isme ek  undefined aa raha hai
+         console.log(this);    //this will print the current context 
+    }
+
 }
 
 console.log(user.usrname);
+console.log(user.getUserDeatailes());   //Method hai to run karana padega sirf reference nahi de sakte hai so we used () 
+console.log(this);           //this will print the empty parenthesis      OR    Global Context
+
+
+
+
+//    **************************Constructor***************************************************  (new    keyword)
+//      constructor function is like ki jab hum usse use karte hai  to vo hume us function ka ek naya instance de deta hai matlab ek nayi copy de deta hai and us copy ke saath hi hum log khelte hai
+
+function User (username, loginCount , isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    return this 
+
+}
+
+const UserOne = User("Rishabh " , 10 , false)
+const UserTwo = User("Khushi" , 23 , true)         //isse userOne ki values overwrite ho jaa rahi hai ,,,,,,,,,,,,,,,, isliye hi hum log 'new'  keyword use karte hai jab bhi hum new constructor function banate hai
+console.log(UserOne);
+
+const UserThree = new User("Rishabh " , 10 , false)
+const UserFour = new User("Khushi" , 23 , true)  
+console.log(UserThree);
+console.log(UserFour); 
+
+//   1st  new keyword use karne ke baad sabse pehle ek empty and naya object create hota hai jissko instance bola jata hai    { empty parenthesis }
+//  2nd   uske baad ek constructor function call hota hai
+//  3rd    "this" keyword ke saare elements or arguments uske andar inject ho jaate hai
+//  4th    you got that in a function 
+
+
+
+
+//****************************prototype  ************************
+
+
+
+
+
+
+
+
+
