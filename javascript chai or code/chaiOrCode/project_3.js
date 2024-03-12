a6e4f2771a1fd471439b1f1246d6142e6c88984f
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2024-03-12 22:49:44 +0530

    added random color project

--- javascript chai or code/chaiOrCode/project_3.js
+++ javascript chai or code/chaiOrCode/project_3.js
@@ -1,11 +1,20 @@
 const clock = document.getElementById('clock')
 //****************** or   document.querryselector('#clock')
+const start = document.getElementById('start')
+const stop = document.getElementById('stop')
 
-
-
-setInterval(function(){
+let timestart
+start.addEventListener('click' , ()=>{
+timestart = setInterval(function(){
     let date = new Date();
 // console.log(date.toLocaleTimeString());.              consoole me nahi chalani
 clock.innerHTML = date.toLocaleTimeString();
 
 }, 1000)
+console.log('time start')
+})
+
+stop.addEventListener('click' , ()=>{
+    clearInterval(timestart)
+    console.log('time stop');
+} )
