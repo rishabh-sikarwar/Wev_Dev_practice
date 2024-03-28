ba602c2388cca885b1d9722734bf85efbec80aa8
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2024-03-28 23:43:58 +0530

    started learning react today

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1> Namaste bhai log </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>  With vite ram ram bhai ko </h1>
+  )
+}
+
+export default App
