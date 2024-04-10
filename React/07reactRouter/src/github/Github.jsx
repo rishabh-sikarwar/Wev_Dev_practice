6aacbe36698e4c72375722c135ef90228489fc28
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2024-04-10 23:23:56 +0530

    context API video 13

--- React/07reactRouter/src/github/Github.jsx
+++ React/07reactRouter/src/github/Github.jsx
@@ -5,15 +5,15 @@ import { useLoaderData } from 'react-router-dom'
 
 
 function Github() {
-        const data = useLoaderData()
+        const dataa = useLoaderData()
 
-
-//     const [data , setData] = useState([])
-//     useEffect(()=>{
-//         fetch('https://api.github.com/users/rishabh-sikarwar')
-//         .then(response => response.json())
-//         .then(data => setData(data))
-//     }, [])
+ 
+    const [data , setData] = useState([])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/rishabh-sikarwar')
+        .then(response => response.json())
+        .then(data => setData(data))
+    }, [])
   return (
    <>
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Folowers : {data.followers}
--- React/07reactRouter/src/main.jsx
+++ React/07reactRouter/src/main.jsx
@@ -46,7 +46,7 @@ import Github from './github/Github.jsx'
         />
         
         
-      </Route>
+      </Route>                                                    
     )
   )
 
@@ -58,3 +58,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider  router={router}/>
   </React.StrictMode>,
 )
+ 
